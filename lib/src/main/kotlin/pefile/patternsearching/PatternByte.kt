13afc125e1491113ddf5117ff58579257d569890
Scui <kaka128@gmx.de>
2023-08-06 22:59:52 +0200

    Add function to construct list of PatternByte from string

--- lib/src/main/kotlin/pefile/patternsearching/PatternByte.kt
+++ lib/src/main/kotlin/pefile/patternsearching/PatternByte.kt
@@ -3,3 +3,12 @@ package patternsearching
 data class PatternByte(val value: UByte, val isWildcard: Boolean = false) {
     fun matches(checkByte: UByte): Boolean = isWildcard || value == checkByte
 }
+
+fun patternBytesFromString(patternString: String): List<PatternByte> {
+    return patternString.split(" ").map {
+        if (it == "?" || it == "??")
+            PatternByte(0u, true)
+        else
+            PatternByte(it.toUByte(16), false)
+    }
+}
